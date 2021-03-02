ed1ff3d3e937486a3351861ff01c7e7ce03fb1e7
wegrzycki_ <wegrzycki_>
2021-03-02 16:15:30 +0100

    :white_check_mark: added tests to :configure method

--- spec/redis_locker_spec.rb
+++ spec/redis_locker_spec.rb
@@ -5,7 +5,21 @@ RSpec.describe RedisLocker do
     expect(RedisLocker::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "configuration by configure with a block" do
+    let(:redis) { instance_double(Redis) }
+    it "allows to pass block" do
+      expect { |b| RedisLocker.configure(&b) }.to yield_with_args
+    end
+    describe "setting redis connection" do
+      context "proper redis client passed" do
+        it "properly saves redis connection" do
+          allow(redis).to receive(:is_a?).with(Redis).and_return(true)
+          RedisLocker.configure do |config|
+            config.redis_connection = redis
+          end
+          expect(RedisLocker.configuration.redis_connection).to eq(redis)
+        end
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "redis_locker"
+require "redis"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
